be155e424964a40aebce5aa22db51448874e1103
harveyWorks <harvey.perater@aclcbutuan.edu.ph>
2025-06-15 10:48:18 +0800

    new features

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap5 import Bootstrap5
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
+bootstrap = Bootstrap5(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
